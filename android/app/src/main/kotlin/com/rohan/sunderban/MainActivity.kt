33f36331ccb1bf19e88a398c935cfcd24d843f89
rohan <rkakar2000@gmail.com>
2020-06-12 22:50:19 +0530

    Created data class

---
+++ android/app/src/main/kotlin/com/rohan/sunderban/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rohan.sunderban
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/database/items.dart
@@ -0,0 +1,10 @@
+class Items{
+  final String item;
+  final String desc;
+  final double price1;
+  final double price2;
+  final double price3;
+  final double mrp;
+
+  Items({this.item,this.desc,this.price1,this.price2,this.price3,this.mrp});
+}
